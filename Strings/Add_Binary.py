ed1f8013951628fc3d6867e8999051c3cc52ecff
Harshitha Priya Marre <181861072+harshithamarre@users.noreply.github.com>
2025-12-25 23:52:55 +0530

    Add python solution for Add Binary LC problem
    Implement binary addition for two input binary numbers.

---
+++ Strings/Add_Binary.py
@@ -0,0 +1,23 @@
+# LC 67
+# Problem Link: https://leetcode.com/problems/add-binary
+
+a = input("Enter first binary number: ")
+b = input("Enter second binary number: ")
+
+res = ""
+carry = 0
+
+a,b = a[::-1], b[::-1]
+
+for i in range(max(len(a),len(b))):
+    digitA = ord(a[i]) - ord("0") if i < len(a) else 0
+    digitB = ord(b[i]) - ord("0") if i < len(b) else 0
+
+    total = digitA + digitB + carry
+    char = str(total % 2)
+    res = char + res
+    carry = total // 2
+
+if carry:
+    res = "1" + res
+print(res)
